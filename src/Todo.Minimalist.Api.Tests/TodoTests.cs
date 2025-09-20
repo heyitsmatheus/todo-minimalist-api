dc55b67664420b812863bdfa3e2ab9c6b992f8b1
Matheus Gonzaga <matheus.gonzagax@gmail.com>
2025-09-20 12:07:44 -0300

    feat: adicionando projeto de testes no ci

---
+++ src/Todo.Minimalist.Api.Tests/TodoTests.cs
@@ -0,0 +1,14 @@
+﻿using Todo.Minimalist.Api.Models;
+
+namespace Todo.Minimalist.Api.Tests;
+
+public class TodoTests
+{
+    [Fact]
+    public void CanCreateTodoItem()
+    {
+        var todo = new TodoItem(Guid.NewGuid(), "Estudar .NET 10", false);
+        Assert.Equal("Estudar .NET 10", todo.Title);
+        Assert.False(todo.IsDone);
+    }
+}

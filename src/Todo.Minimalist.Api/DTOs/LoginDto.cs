3a72cefdac28755c9f34c9bef2abee436366b12e
Matheus Gonzaga <matheus.gonzagax@gmail.com>
2025-09-24 20:55:04 -0300

    feat: implementando login e ajustes para melhor escalabilidade do código

--- src/Todo.Minimalist.Api/DTOs/LoginDto.cs
+++ src/Todo.Minimalist.Api/DTOs/LoginDto.cs
@@ -1,7 +1,8 @@
-﻿namespace Todo.Minimalist.Api.DTOs;
+﻿
+namespace Todo.Minimalist.Api.DTOs;
+
 public class LoginDto
 {
     public string Username { get; set; } = "";
     public string Password { get; set; } = "";
-
 }
